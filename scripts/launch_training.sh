fb0e7bc709ec1537b85d215e413ac065f47912b9
rflor01 <rafael.flor@edu.uah.es>
2023-11-14 10:37:14 +0100

    100000

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -20,6 +20,5 @@ python -u -m torch.distributed.launch \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 100000 \
-    NUM_ENVIRONMENTS 60 \
-    RL.DDPPO.force_distributed True \
-    TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
+    NUM_ENVIRONMENTS 30 \
+
