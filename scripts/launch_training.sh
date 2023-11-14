b93090b574f09ce0dc41a22e25f4b4780fd65dc8
rflor01 <rafael.flor@edu.uah.es>
2023-11-14 11:15:51 +0100

    100000

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -17,5 +17,10 @@ python -u -m torch.distributed.launch \
     run.py \
     --exp-config $config \
     --run-type train \
+    TENSORBOARD_DIR $TENSORBOARD_DIR \
+    CHECKPOINT_FOLDER $CHECKPOINT_DIR \
+    NUM_UPDATES 100000 \
+    NUM_ENVIRONMENTS 30 \
+    EVAL USE_CKPT_CONFIG False\
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
