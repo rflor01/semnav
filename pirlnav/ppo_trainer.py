f848e3fb93bbaa0676a6e50ef2650fc8246445b1
rflor01 <rafael.flor@edu.uah.es>
2023-11-16 10:09:23 +0100

    100000

--- pirlnav/ppo_trainer.py
+++ pirlnav/ppo_trainer.py
@@ -147,7 +147,7 @@ class PIRLNavPPOTrainer(PPOTrainer):
             start_critic_warmup_at=self.config.RL.Finetune.start_critic_warmup_at,
         )
 
-        resume_state = load_resume_state(self.config)
+        resume_state = load_resume_state('data/il_ckpts/ckpt.9.pth')
         if resume_state is not None:
             self.agent.load_state_dict(resume_state["state_dict"])
             self.agent.optimizer.load_state_dict(resume_state["optim_state"])
