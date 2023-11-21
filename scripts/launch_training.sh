787ac39b22b06062b280582ef89744bd66f1da5d
rflor01 <rafael.flor@edu.uah.es>
2023-11-21 11:14:13 +0100

    I am very confused

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -6,8 +6,8 @@ export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/il_objectnav.yaml"
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/300k_inflection"
-CHECKPOINT_DIR="data/il_ckpts_300k_inflection"
+TENSORBOARD_DIR="tb/300k_inflection2"
+CHECKPOINT_DIR="data/il_ckpts_300k_inflection2"
 INFLECTION_COEF=3.234951275740812
 
 echo "In ObjectNav IL DDP"
