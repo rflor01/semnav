e59910e3a39922684abf72adcdf21605cbc25e9a
rflor01 <rafael.flor@edu.uah.es>
2023-11-21 11:40:10 +0100

    I am very confused

--- pirlnav/task/sensors.py
+++ pirlnav/task/sensors.py
@@ -90,7 +90,6 @@ class InflectionWeightSensor(Sensor):
             != episode.reference_replay[self.timestep].action
         ):
             inflection_weight = self._config.INFLECTION_COEF
-            print(inflection_weight)
         self.timestep += 1
         return inflection_weight
 
