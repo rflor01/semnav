562a6216ebaad3698302ec23778bc0b56e57e7d0
rflor01 <rafael.flor@edu.uah.es>
2023-12-05 13:44:51 +0100

    Eval stable ckpt mini2 lr 0001 env8

--- scripts/launch_training_eval.sh
+++ scripts/launch_training_eval.sh
@@ -5,7 +5,7 @@ export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/il_objectnav.yaml"
-DATA_PATH="data/datasets/objectnav/objectnav_hm3d_v1"
+DATA_PATH="data/datasets/objectnav/hm3d/v1"
 TENSORBOARD_DIR="tb/final_8env_mini2/"
 CHECKPOINT_DIR="data/data/final_8env_mini2"
 
