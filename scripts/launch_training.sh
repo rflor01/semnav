deac0da1049f6e71760d0ffaeb49365a09df84fe
rflor01 <rafael.flor@edu.uah.es>
2024-02-19 09:30:58 +0100

    Change num minibatch

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -20,7 +20,8 @@ python -u -m torch.distributed.launch \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 320000 \
-    NUM_ENVIRONMENTS 2 \
+    NUM_ENVIRONMENTS 1 \
+    IL.num_mini_batch 1\
     EVAL.USE_CKPT_CONFIG True\
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
