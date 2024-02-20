5ace39f5750adc1a29ee804b252e8320972c23f0
rflor01 <rafael.flor@edu.uah.es>
2024-02-20 14:02:32 +0100

    dgx changes 14 envs

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -20,7 +20,7 @@ python -u -m torch.distributed.launch \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 320000 \
-    NUM_ENVIRONMENTS 4 \
+    NUM_ENVIRONMENTS 14 \
     IL.BehaviorCloning.num_mini_batch 2\
     EVAL.USE_CKPT_CONFIG True\
     RL.DDPPO.force_distributed True \
