463498cc810c90a25495f6a795db2945a0abf36d
rflor01 <rafael.flor@edu.uah.es>
2024-02-20 13:08:21 +0100

    dgx changes

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export NUM_GPUS=4
+export NUM_GPUS=2
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 export HABITAT_SIM_LOG=quiet
 
 config="configs/experiments/il_objectnav.yaml"
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/data/first_rgb_semantic"
-CHECKPOINT_DIR="data/first_rgb_semantic_ckpt"
+TENSORBOARD_DIR="tb/data/second_rgb_semantic"
+CHECKPOINT_DIR="data/second_rgb_semantic_ckpt"
 INFLECTION_COEF=3.234951275740812
 
 echo "In ObjectNav IL DDP"
@@ -20,8 +20,8 @@ python -u -m torch.distributed.launch \
     TENSORBOARD_DIR $TENSORBOARD_DIR \
     CHECKPOINT_FOLDER $CHECKPOINT_DIR \
     NUM_UPDATES 320000 \
-    NUM_ENVIRONMENTS 1 \
-    IL.BehaviorCloning.num_mini_batch 1\
+    NUM_ENVIRONMENTS 26 \
+    IL.BehaviorCloning.num_mini_batch 2\
     EVAL.USE_CKPT_CONFIG True\
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
