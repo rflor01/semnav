cdde58a2b0a5bc6e1c2fda582dd5c4ac497413aa
rafa <rafael.flor@edu.uah.es>
2024-07-09 10:58:14 +0200

    Checking dgx semantic

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -18,8 +18,8 @@ export OMP_NUM_THREADS=$((num_cpus/num_gpus))
 
 config="configs/experiments/il_objectnav.yaml"
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/new_modified_dataset_24envs"
-CHECKPOINT_DIR="data/checkpoints/new_modified_dataset_24envs"
+TENSORBOARD_DIR="tb/debug"
+CHECKPOINT_DIR="data/checkpoints/debug"
 INFLECTION_COEF=3.234951275740812
 
 echo "In ObjectNav IL DDP"
