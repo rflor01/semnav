5d3caaa7f8ae0a02d1018dd41f4a45ff82f1d99f
rafa <rafael.flor@edu.uah.es>
2024-07-15 00:01:48 +0200

    Trying to get images in dgx

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -32,7 +32,7 @@ torchrun --nproc_per_node $num_gpus run.py \
     NUM_UPDATES 320000 \
     NUM_ENVIRONMENTS 24 \
     IL.BehaviorCloning.num_mini_batch 2\
-    EVAL.USE_CKPT_CONFIG True\
+    EVAL.USE_CKPT_CONFIG False\
     RL.DDPPO.force_distributed True \
     TASK_CONFIG.DATASET.DATA_PATH "$DATA_PATH/{split}/{split}.json.gz" \
     TASK_CONFIG.TASK.INFLECTION_WEIGHT_SENSOR.INFLECTION_COEF $INFLECTION_COEF \
