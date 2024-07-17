0fd26a9603a8bd13decc9bd0d115793655a3a3cf
rafa <rafael.flor@edu.uah.es>
2024-07-17 18:41:10 +0200

    Training without pretrained encoder in zeus

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -18,8 +18,8 @@ export OMP_NUM_THREADS=$((num_cpus/num_gpus))
 
 config="configs/experiments/il_objectnav.yaml"
 DATA_PATH="data/datasets/objectnav/objectnav_hm3d/objectnav_hm3d_hd"
-TENSORBOARD_DIR="tb/debug3"
-CHECKPOINT_DIR="data/checkpoints/debug3"
+TENSORBOARD_DIR="tb/no_pretrained_encoder_zeuslr0.0001"
+CHECKPOINT_DIR="data/checkpoints/no_pretrained_encoder_zeuslr0.0001"
 INFLECTION_COEF=3.234951275740812
 
 
