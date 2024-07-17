5c1bdc9e239563a31b7c733a37801bcffbdd19a2
rafa <rafael.flor@edu.uah.es>
2024-07-17 10:13:18 +0200

    Works in zeus??

--- scripts/launch_training.sh
+++ scripts/launch_training.sh
@@ -17,7 +17,7 @@ export HABITAT_SIM_LOG=quiet
 export OMP_NUM_THREADS=$((num_cpus/num_gpus))
 
 config="configs/experiments/il_objectnav.yaml"
-DATA_PATH="data/datasets/objectnav/objectnav_hm3d_hd"
+DATA_PATH="data/datasets/objectnav/objectnav_hm3d/objectnav_hm3d_hd"
 TENSORBOARD_DIR="tb/debug3"
 CHECKPOINT_DIR="data/checkpoints/debug3"
 INFLECTION_COEF=3.234951275740812
